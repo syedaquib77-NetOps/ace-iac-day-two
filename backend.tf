4a4dcdfa7e8a20509cc5a5259b46de425073c758
syedaquib77-DevOps <106496754+syedaquib77-DevOps@users.noreply.github.com>
2022-05-30 05:17:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-     organization = "BT-Azure"
+    organization = "syedaquib77"
     workspaces {
       name = "ace-iac-day-two"
     }
